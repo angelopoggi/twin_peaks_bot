f318a45ba49c14e32791483f67f313a47bdcb71c
Angelo Poggi <angelopoggi@Angelo-Poggi-Remote-Laptop.local>
2021-07-05 13:13:04 -0400

    initial commit of beta branch

---
+++ cole_class.py
@@ -0,0 +1,10 @@
+#Main Class file
+#2021 - Angelo Poggi
+
+
+class Cole:
+    def agent_cole(self, instance, speak):
+        self.start = instance
+        self.speak = speak
+
+    def bot_login(self):
